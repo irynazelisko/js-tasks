6c5c88b718bf90184215dba046a997812cdb3c1d
Iryna <irazelisko@gmail.com>
2023-02-23 22:25:48 +0200

    lesson-21

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = prompt('Enter first number')
+secondNumber = prompt('Enter second number')
+
+alert('Sum is ' + " " + (Number(firstNumber) + Number(secondNumber)))
+alert('Subtraction is' + " " + (firstNumber - secondNumber))
+alert('Divide is' + " " + (firstNumber / secondNumber))
+alert('Multiply is' + " " + (firstNumber * secondNumber))
