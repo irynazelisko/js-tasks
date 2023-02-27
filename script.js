6ed99e4dd4bae2960b68c7c0759ff21cded9ea17
Iryna <irazelisko@gmail.com>
2023-02-27 23:08:13 +0200

    lesson-22

--- script.js
+++ script.js
@@ -4,7 +4,28 @@ let secondNumber;
 firstNumber = prompt('Enter first number')
 secondNumber = prompt('Enter second number')
 
+
+if (firstNumber == '' || secondNumber == ''){
+    alert('Поле не може бути пустим! Будь ласка введіть число!')
+}
+
+if (secondNumber == 0) {
+    alert('Error. Cannot divide by zero')
+}else{
+    alert('Divide is' + " " + (firstNumber / secondNumber))
+}
+
+
+if (firstNumber > secondNumber) {
+   if (confirm('Ви впевнені що хочете продовжити операцію?')){
+    alert('Subtraction is' + " " + (firstNumber - secondNumber))
+   } 
+}
+
 alert('Sum is ' + " " + (Number(firstNumber) + Number(secondNumber)))
-alert('Subtraction is' + " " + (firstNumber - secondNumber))
-alert('Divide is' + " " + (firstNumber / secondNumber))
 alert('Multiply is' + " " + (firstNumber * secondNumber))
+
+
+
+
+
