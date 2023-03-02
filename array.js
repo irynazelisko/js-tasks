2f6618fe70438178e2ea59cd380408a6967377b3
Iryna <irazelisko@gmail.com>
2023-03-02 13:39:21 +0200

    lesson-23

---
+++ array.js
@@ -0,0 +1,38 @@
+/*пошук суми елементів масиву*/ 
+let numbers = [2,34,45,10,3,99,5,44,11,29];
+let sumOfArray = 0;
+
+for(let i = 0; i < numbers.length; i++){
+    if (typeof i === 'number') {
+        sumOfArray += numbers[i]
+}
+}
+console.log(`Sum of array is ${sumOfArray}`)
+
+/* максимальне значення */ 
+let max = numbers[0];
+for (let i = 1; i < numbers.length; ++i) {
+  if (numbers[i] > max && typeof i === 'number') {
+    max = numbers[i];
+  }
+}
+console.log(`Minimum value is ${max}`)
+
+/* мінімальне значення */
+
+let min = numbers[0];
+for (let i = 1; i < numbers.length; ++i) {
+  if (numbers[i] < min && typeof i === 'number') {
+    min = numbers[i];
+  }
+}
+console.log(`Minimum value is ${min}`)
+
+
+/* пʼять # */ 
+var str = " ";
+var symbol = "#";
+for (var i = 0; i < 5; i++) {
+  str += symbol;
+  console.log(str);
+}
