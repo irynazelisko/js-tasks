5e0d4f7848bad50e3e4330e521d427a1f5c13ad3
Iryna <irazelisko@gmail.com>
2023-03-07 18:10:14 +0200

    lesson-24

--- array.js
+++ array.js
@@ -1,37 +1,33 @@
-/*пошук суми елементів масиву*/ 
-let numbers = [2,34,45,10,3,99,5,44,11,29];
-let sumOfArray = 0;
-
-for(let i = 0; i < numbers.length; i++){
-    if (typeof i === 'number') {
-        sumOfArray += numbers[i]
-}
-}
-console.log(`Sum of array is ${sumOfArray}`)
 
 /* максимальне значення */ 
-let max = numbers[0];
-for (let i = 1; i < numbers.length; ++i) {
-  if (numbers[i] > max && typeof i === 'number') {
-    max = numbers[i];
+function findMaxValue(array) {
+
+  let max = array[0];
+
+for (let i = 1; i < array.length; ++i) {
+  if (array[i] > max) {
+    max = array[i];
   }
 }
-console.log(`Minimum value is ${max}`)
+return max;
+
+}
+console.log(findMaxValue([2,34,45,10,3,99,5,44,11,333,4576,29]))
+
 
 /* мінімальне значення */
+function findMinValue(array) {
 
-let min = numbers[0];
-for (let i = 1; i < numbers.length; ++i) {
-  if (numbers[i] < min && typeof i === 'number') {
-    min = numbers[i];
+let min = array[0];
+
+for (let i = 1; i < array.length; ++i) {
+  if (array[i] < min) {
+    min = array[i];
   }
-}
-console.log(`Minimum value is ${min}`)
+} 
 
-/* пʼять # */ 
-let str = " ";
-let symbol = "#";
-for (let i = 0; i < 5; i++) {
-  str += symbol;
-  console.log(str);
+return min;
+
 }
+console.log(findMinValue([-1,34,54,55,21,23,98,-18,4,1,2]))
+
--- script.js
+++ script.js
@@ -1,29 +1,55 @@
-let firstNumber;
-let secondNumber;
-
-firstNumber = prompt('Enter first number')
-secondNumber = prompt('Enter second number')
-
-
-if (firstNumber == '' || secondNumber == ''){
-    alert('Поле не може бути пустим! Будь ласка введіть число!')
+   /* divide */ 
+function divide(firstNumber,secondNumber) {
+if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+    return 'Error. Write numbers,please!'
+}
+if (secondNumber == 0 ) {
+    return 'Error. Cannot divide by zero'
+}
+return result = firstNumber / secondNumber;
 }
 
-if (secondNumber == 0) {
-    alert('Error. Cannot divide by zero')
-}else{
-    alert('Divide is' + " " + (firstNumber / secondNumber))
+console.log(divide(24,4))
+console.log(divide(15,0))
+console.log(divide('twenty',10))
+
+
+    /* substraction */
+function subtraction(firstNumber,secondNumber) {
+if (firstNumber < secondNumber) {
+    return 'Error.You cannot subtract a smaller number from a larger number!'
+}
+if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+    return 'Error. Write numbers,please!'
+}
+return result = firstNumber - secondNumber
 }
 
+console.log(subtraction(4,10))
+console.log(subtraction('ten',5))
+console.log(subtraction(24,4))
 
-if (firstNumber > secondNumber) {
-   if (confirm('Ви впевнені що хочете продовжити операцію?')){
-    alert('Subtraction is' + " " + (firstNumber - secondNumber))
-   } 
+       /* sum */
+
+function sum(firstNumber,secondNumber) {
+    if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+        return 'Error. Write numbers,please!'
+    }
+ return result = firstNumber + secondNumber
 }
 
-alert('Sum is ' + " " + (Number(firstNumber) + Number(secondNumber)))
-alert('Multiply is' + " " + (firstNumber * secondNumber))
+ console.log(sum(23,40))
+ console.log(sum('fourty one', 41))
+
+       /* multiply */ 
+function multiply(firstNumber,secondNumber){
+    if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+        return 'Error. Write numbers,please!'
+    }
+    return result = firstNumber * secondNumber
+}
+console.log(multiply(20,4))
+console.log(multiply('four',4))
 
 
 
