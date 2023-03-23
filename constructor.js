848b6b4fdfca0e6ea32856b959d9a58fbd15e32e
Iryna <irazelisko@gmail.com>
2023-03-23 18:46:00 +0200

    lesson-26

---
+++ constructor.js
@@ -0,0 +1,43 @@
+
+      /* Accumulator */
+
+function Accumulator(initialValue) {
+   this.value = initialValue;
+    
+ Accumulator.prototype.increment = function() {
+    this.value++;
+    console.log(`${this.value}`)
+ }
+
+
+ Accumulator.prototype.decrement = function() {
+    this.value--;
+    console.log(`${this.value}`)
+ }
+  }
+
+  const acc = new Accumulator(24);
+  acc.increment();
+  acc.decrement();
+ 
+  
+        /* Cancelable Accumulator */
+
+  function CancelableAccumulator(initialValue) {
+     Accumulator.call(this, initialValue)
+     
+
+  this.clear = function() {
+      this.value = initialValue;
+      console.log(`${this.value}`)
+      }
+
+    }
+    CancelableAccumulator.prototype = Object.create(Accumulator.prototype)
+    
+  const cancelAcc = new CancelableAccumulator(20);
+    cancelAcc.increment(); 
+    cancelAcc.increment();
+    cancelAcc.decrement(); 
+    cancelAcc.clear();
+ 
