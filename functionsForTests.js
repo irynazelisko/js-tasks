b06b4676afb0dbffa5c6a4f65881f8e923decf03
Iryna <irazelisko@gmail.com>
2023-04-08 20:00:16 +0300

    lesson-30

---
+++ functionsForTests.js
@@ -0,0 +1,53 @@
+/* додавання */
+
+function sum(firstNumber, secondNumber) {
+    if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+        return 'Error. Write numbers,please!'
+    }
+    return result = firstNumber + secondNumber
+}
+
+
+// /* віднімання */
+
+// function subtraction(firstNumber, secondNumber) {
+//     if (firstNumber < secondNumber) {
+//         return 'Error.You cannot subtract a smaller number from a larger number!'
+//     }
+//     if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+//         return 'Error. Write numbers,please!'
+//     }
+//     return result = firstNumber - secondNumber
+// }
+
+// console.log(subtraction(4, 10))
+// console.log(subtraction('ten', 5))
+// console.log(subtraction(28, 4))
+
+
+// /* ділення */
+
+// function divide(firstNumber, secondNumber) {
+//     if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+//         return 'Error. Write numbers,please!'
+//     }
+//     if (secondNumber == 0) {
+//         return 'Error. Cannot divide by zero'
+//     }
+//     return result = firstNumber / secondNumber;
+// }
+
+// console.log(divide(24, 4))
+// console.log(divide(15, 0))
+// console.log(divide('twenty', 10))
+
+// /* множення */
+
+// function multiply(firstNumber, secondNumber) {
+//     if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+//         return 'Error. Write numbers,please!'
+//     }
+//     return result = firstNumber * secondNumber
+// }
+// console.log(multiply(20, 4))
+// console.log(multiply('four', 4))
---
+++ index.test.js
@@ -0,0 +1,16 @@
+describe("Get sum of numbers", () => {
+  const desc = "<p>Get sum of numbers</p>";
+
+  describe(desc, () => {
+      it("should return 60", () => {
+          const result = sum(20,40);
+          expect(result).toBe(60);
+      })
+  })
+})
+
+
+
+
+
+   
