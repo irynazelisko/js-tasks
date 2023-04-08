ecf51fcd7fed7a26e6306b60293fda824a54325c
Iryna <irazelisko@gmail.com>
2023-04-08 20:09:56 +0300

    trying to push changes

--- functionsForTests.js
+++ functionsForTests.js
@@ -1,3 +1,5 @@
+/* LESSON 24 */
+
 /* додавання */
 
 function sum(firstNumber, secondNumber) {
@@ -10,44 +12,128 @@ function sum(firstNumber, secondNumber) {
 
 // /* віднімання */
 
-// function subtraction(firstNumber, secondNumber) {
-//     if (firstNumber < secondNumber) {
-//         return 'Error.You cannot subtract a smaller number from a larger number!'
-//     }
-//     if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
-//         return 'Error. Write numbers,please!'
-//     }
-//     return result = firstNumber - secondNumber
-// }
-
-// console.log(subtraction(4, 10))
-// console.log(subtraction('ten', 5))
-// console.log(subtraction(28, 4))
+function subtraction(firstNumber, secondNumber) {
+    if (firstNumber < secondNumber) {
+        return 'Error.You cannot subtract a smaller number from a larger number!'
+    }
+    if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+        return 'Error. Write numbers,please!'
+    }
+    return result = firstNumber - secondNumber
+}
 
 
 // /* ділення */
 
-// function divide(firstNumber, secondNumber) {
-//     if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
-//         return 'Error. Write numbers,please!'
-//     }
-//     if (secondNumber == 0) {
-//         return 'Error. Cannot divide by zero'
-//     }
-//     return result = firstNumber / secondNumber;
-// }
+function divide(firstNumber, secondNumber) {
+    if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+        return 'Error. Write numbers,please!'
+    }
+    if (secondNumber == 0) {
+        return 'Error. Cannot divide by zero'
+    }
+    return result = firstNumber / secondNumber;
+}
 
-// console.log(divide(24, 4))
-// console.log(divide(15, 0))
-// console.log(divide('twenty', 10))
 
 // /* множення */
 
-// function multiply(firstNumber, secondNumber) {
-//     if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
-//         return 'Error. Write numbers,please!'
-//     }
-//     return result = firstNumber * secondNumber
-// }
-// console.log(multiply(20, 4))
-// console.log(multiply('four', 4))
+function multiply(firstNumber, secondNumber) {
+    if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+        return 'Error. Write numbers,please!'
+    }
+    return result = firstNumber * secondNumber
+}
+
+/* максимальне значення */
+function findMaxValue(array) {
+
+    let max = array[0];
+
+    for (let i = 1; i < array.length; ++i) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+
+}
+console.log(findMaxValue([2, 34, 45, 10, 3, 59, 5, 44, 11, 333, 4576, 29]))
+
+/* мінімальне значення */
+function findMinValue(array) {
+
+    let min = array[0];
+
+    for (let i = 1; i < array.length; ++i) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+
+    return min;
+
+}
+console.log(findMinValue([-1, 34, 54, 55, 21, 43, 98, -18, 4, 1, 2]))
+
+/* LESSON 26 */
+
+/* Accumulator */
+
+function Accumulator(initialValue) {
+    this.value = initialValue;
+}
+
+Accumulator.prototype.increment = function () {
+    this.value++;
+    console.log(`${this.value}`)
+}
+
+
+Accumulator.prototype.decrement = function () {
+    this.value--;
+    console.log(`${this.value}`)
+}
+
+
+const acc = new Accumulator(24);
+acc.increment();
+acc.decrement();
+
+/* Cancelable Accumulator */
+
+function CancelableAccumulator(startingValue) {
+    Accumulator.call(this, startingValue);
+    this.startingValue = startingValue;
+}
+
+CancelableAccumulator.prototype = Object.create(Accumulator.prototype);
+CancelableAccumulator.prototype.constructor = CancelableAccumulator;
+
+CancelableAccumulator.prototype.clear = function () {
+    this.value = this.startingValue;
+};
+
+/* LESSON 28 */
+/* toggle  image */
+
+function toggleImageSize() {
+    const image = document.getElementById("image");
+    image.classList.toggle("image-enlarged");
+    image.style.transform = image.classList.contains("image-enlarged") ? "scale(1.2)" : "scale(1)";
+}
+
+/* scroll to sections */
+
+function scrollToSection(event) {
+    if (event.key === "a") {
+        return "sectionA";
+    } else if (event.key === "b") {
+        return "sectionB";
+    } else if (event.key === "c") {
+        return "sectionC";
+    } else if (event.key === "d") {
+        return "sectionD";
+    }
+}
+
--- index.test.js
+++ index.test.js
@@ -1,16 +1,169 @@
+
+/* LESSON 24 */
 describe("Get sum of numbers", () => {
   const desc = "<p>Get sum of numbers</p>";
 
   describe(desc, () => {
-      it("should return 60", () => {
-          const result = sum(20,40);
-          expect(result).toBe(60);
-      })
+    it("should return 60", () => {
+      const result = sum(20, 40);
+      expect(result).toBe(60);
+    });
+  });
+});
+
+describe("Get subtraction of numbers", () => {
+  const desc = "<p>Get subtraction of numbers</p>";
+
+  describe(desc, () => {
+    it("should return 24", () => {
+      const result = subtraction(28, 4);
+      expect(result).toBe(24);
+    });
+  });
+});
+
+describe("Get divide of numbers", () => {
+  const desc = "<p>Get divide of numbers</p>";
+
+  describe(desc, () => {
+    it("should return 9", () => {
+      const result = divide(36, 4);
+      expect(result).toBe(9);
+    });
+  });
+});
+
+describe("Get multiply of numbers", () => {
+  const desc = "<p>Get multiply of numbers</p>";
+
+  describe(desc, () => {
+    it("should return 80", () => {
+      const result = multiply(20, 4);
+      expect(result).toBe(80);
+    });
+  });
+});
+/* пошук максимального значення */
+describe('Find max value', () => {
+  const desc = "<p>Find max value</p>";
+
+  describe(desc, () => {
+    it('should return 4576', () => {
+      const result = findMaxValue([2, 34, 45, 10, 3, 59, 5, 44, 11, 333, 4576, 29]);
+      expect(result).toBe(4576);
+    });
+  });
+});
+
+
+/* пошук мінімального значення */
+describe('Find minimum value', () => {
+  const desc = "<p>Find minimum value</p>";
+
+  describe(desc, () => {
+    it('should return -18', () => {
+      const result = findMinValue([-1, 34, 54, 55, 21, 43, 98, -18, 4, 1, 2]);
+      expect(result).toBe(-18);
+    });
+  });
+});
+
+
+/* LESSON 26 */
+
+describe('Checking increment value', () => {
+  const desc = "<p>Checking increment value</p>";
+
+  describe(desc, () => {
+    it('should return 25', () => {
+      let acc = new Accumulator(24);
+      acc.increment();
+      expect(acc.value).toBe(25);
+    });
+  });
+});
+
+describe('Checking decrement value', () => {
+  const desc = "<p>Checking decrement value</p>";
+
+  describe(desc, () => {
+    it('should return 23', () => {
+      let acc = new Accumulator(24);
+      acc.decrement();
+      expect(acc.value).toBe(23);
+    });
+  });
+});
+
+describe('Checking clear method ', () => {
+  const desc = "<p>Checking clear method </p>";
+
+  describe(desc, () => {
+    it('should return 24', () => {
+      let cncblAcc = new CancelableAccumulator(24);
+      cncblAcc.clear();
+      expect(cncblAcc.value).toBe(24);
+    });
+  });
+});
+
+/* LESSON 28 */
+
+describe("Toggle image on 'click", () => {
+  let desc = "<p>Toggle image on 'click' </p>";
+
+  describe(desc, () => {
+    const image = document.createElement("img");
+    image.setAttribute("id", "image");
+    document.body.appendChild(image);
+
+    it('should toggle the "image-enlarged" class', () => {
+
+      spyOn(image.classList, "toggle");
+      toggleImageSize();
+      expect(image.classList.toggle).toHaveBeenCalledWith("image-enlarged");
+    });
+    it('should increase the image size', () => {
+
+      image.style.transform = "scale(1)";
+      toggleImageSize();
+      expect(image.style.transform).toEqual("scale(1.2)");
+    });
+
+    it('should descrese the image size', () => {
+      image.style.transform = "scale(1.2)";
+      toggleImageSize();
+      expect(image.style.transform).toEqual("scale(1)");
+    });
+  })
+})
+
+
+describe('Scroll to sections', () => {
+  const desc = "<p>Scroll to sections </p>";
+
+  describe(desc, () => {
+    it('should return "sectionA" for section A', () => {
+      const event = new KeyboardEvent("keydown", { key: "a" });
+      expect(scrollToSection(event)).toBe("sectionA");
+    })
+    it('should return "sectionB" for section B', () => {
+      const event = new KeyboardEvent("keydown", { key: "b" });
+      expect(scrollToSection(event)).toBe("sectionB");
+    });
+
+    it('should return "sectionC" for section C', () => {
+      const event = new KeyboardEvent("keydown", { key: "c" });
+      expect(scrollToSection(event)).toBe("sectionC");
+    });
+
+    it('should return "sectionD" for section D', () => {
+      const event = new KeyboardEvent("keydown", { key: "d" });
+      expect(scrollToSection(event)).toBe("sectionD");
+    });
   })
 })
 
 
 
 
-
-   
